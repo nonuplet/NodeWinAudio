c7b5f2a9707309c3767f30f608938732bbae9701
Kokonoe <130939038+nonuplet@users.noreply.github.com>
2025-02-25 11:00:35 +0900

    First commit

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JavaScript.NodeApi;
+
+namespace NodeWinAudio;
+
+[JSExport]
+public static class HelloWorld
+{
+    public static string SayHello(string name)
+    {
+        return $"Hello from {name}!";
+    }
+}
---
+++ ts/index.ts
@@ -0,0 +1,7 @@
+﻿import * as dotnet from "node-api-dotnet"
+import type * as NodeWinAudio from "../bin/NodeWinAudio"
+
+console.log("hello from typescript!");
+
+const nodeWinAudio = dotnet.require("./bin/NodeWinAudio") as typeof NodeWinAudio;
+console.log(nodeWinAudio.HelloWorld.sayHello(".NET"));
