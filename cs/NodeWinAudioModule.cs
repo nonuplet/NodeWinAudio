375b7ba7a3093bc3190e760d3e8f59b1b92692c9
Kokonoe <130939038+nonuplet@users.noreply.github.com>
2025-02-27 16:30:21 +0900

    Add GetVolume() and update .csproj

---
+++ cs/NodeWinAudioModule.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.JavaScript.NodeApi;
+using NAudio.CoreAudioApi;
+
+namespace NodeWinAudio;
+
+[JSExport]
+public static class NodeWinAudioModule
+{
+  public static List<string> GetDevices()
+  {
+    var devices = new List<string>();
+    using var enumerator = new MMDeviceEnumerator();
+    var endpoints = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active);
+    foreach (var device in endpoints)
+    {
+      devices.Add(device.FriendlyName);
+      device.Dispose();
+    }
+
+    return devices;
+  }
+
+  public static float GetVolume()
+  {
+    using var enumerator = new MMDeviceEnumerator();
+    var device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
+    using var volume = device.AudioEndpointVolume;
+    return volume.MasterVolumeLevelScalar;
+  }
+}
--- ts/index.ts
+++ ts/index.ts
@@ -1,9 +1,14 @@
-﻿import * as dotnet from "node-api-dotnet";
-import type * as NodeWinAudio from "../bin/NodeWinAudio";
+﻿import * as dotnet from "node-api-dotnet"
+import type * as NodeWinAudio from "../bin/NodeWinAudio"
 
-console.log("hello from typescript!");
+console.log("hello from typescript!")
 
-const nodeWinAudio = dotnet.require(
-  "./bin/NodeWinAudio",
-) as typeof NodeWinAudio;
-console.log(nodeWinAudio.HelloWorld.sayHello(".NET"));
+const nodeWinAudio = dotnet.require("./bin/NodeWinAudio") as typeof NodeWinAudio
+console.log("----- devices -----")
+console.log(nodeWinAudio.NodeWinAudioModule.getDevices())
+console.log("----- volume -----")
+try {
+  console.log(nodeWinAudio.NodeWinAudioModule.getVolume() * 100)
+} catch (e) {
+  console.error(e)
+}
