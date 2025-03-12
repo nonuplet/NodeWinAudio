ed08ed99bab8e11cda27d0c3635adcb488af01e7
Kokonoe <130939038+nonuplet@users.noreply.github.com>
2025-03-12 16:33:43 +0900

    Add Tests for callback

--- tests/NodeWinAudio.test.ts
+++ tests/NodeWinAudio.test.ts
@@ -1,4 +1,5 @@
 ﻿import { NodeWinAudio } from "../ts/NodeWinAudio"
+import { JSVolumeNotification } from "../bin/NodeWinAudio"
 
 test("get device info", () => {
   expect(NodeWinAudio.getAllDevices()).toBeDefined()
@@ -32,4 +33,39 @@ test("set mute", () => {
   NodeWinAudio.setDefaultDeviceMuted(currentMuteState) // reset
 
   expect(deviceMuteState).toEqual(newMuteState)
+})
+
+test("register callback", async () => {
+  const currentVolume = NodeWinAudio.getDefaultDeviceVolume()
+  const newVolume = Math.floor(Math.random() * 100) * 0.01
+
+  const mockCallback = jest.fn((params: JSVolumeNotification) => {
+    console.log(`${params.name}, ${params.masterVolume}, ${params.muted}, ${params.type}`)
+  })
+
+  // register check
+  NodeWinAudio.registerVolumeChangeCallback(mockCallback)
+  NodeWinAudio.setDefaultDeviceVolume(newVolume)
+  await new Promise((r) => setTimeout(r, 500))
+  expect(mockCallback).toHaveBeenCalledTimes(1)
+
+  // unregister check
+  NodeWinAudio.unregisterVolumeChangeCallback(mockCallback)
+  NodeWinAudio.setDefaultDeviceVolume(newVolume)
+  await new Promise((r) => setTimeout(r, 500))
+  expect(mockCallback).toHaveBeenCalledTimes(1)
+
+  // clear check
+  const mockCallback2 = jest.fn((params: JSVolumeNotification) => {
+    console.log(`${params.name}, ${params.masterVolume}, ${params.muted}, ${params.type}`)
+  })
+  NodeWinAudio.registerVolumeChangeCallback(mockCallback)
+  NodeWinAudio.registerVolumeChangeCallback(mockCallback2)
+  NodeWinAudio.clearVolumeChangeCallback()
+  await new Promise((r) => setTimeout(r, 500))
+  NodeWinAudio.setDefaultDeviceVolume(newVolume)
+  expect(mockCallback).toHaveBeenCalledTimes(1)
+  expect(mockCallback2).toHaveBeenCalledTimes(0)
+
+  NodeWinAudio.setDefaultDeviceVolume(currentVolume) // Reset
 })
